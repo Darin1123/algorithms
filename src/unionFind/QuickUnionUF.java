d60a2c09434df1dc83d1ab0b74d3343f8adc0356
Zefeng Wang <darin1123@outlook.com>
2021-01-27 02:35:39 +0800

    add quick union implementation

---
+++ src/unionFind/QuickUnionUF.java
@@ -0,0 +1,33 @@
+package unionFind;
+
+/**
+ * quick union implementation
+ */
+public class QuickUnionUF extends UF {
+
+    /**
+     * The constructor of UF
+     *
+     * @param N the size of the pool
+     */
+    public QuickUnionUF(int N) {
+        super(N);
+    }
+
+    @Override
+    public int find(int p) {
+        while (p != id[p]) p = id[p];
+        return p;
+    }
+
+    @Override
+    public void union(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+        if (pRoot == qRoot) return;
+
+        id[pRoot] = qRoot;
+
+        count--;
+    }
+}

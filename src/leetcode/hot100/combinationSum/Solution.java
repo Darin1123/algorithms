f6d1b1838f99ab956bd360f33d5ed2ea3c9813cc
Zefeng Wang <darin1123@outlook.com>
2021-07-29 21:44:21 +0800

    leetcode trap [hard]

--- src/leetcode/hot100/combinationSum/Solution.java
+++ src/leetcode/hot100/combinationSum/Solution.java
@@ -5,9 +5,7 @@ import java.util.List;
 
 /**
  * 给定一个无重复元素的正整数数组 candidates 和一个正整数 target，找出 candidates 中所有可以使数字和为目标数 target 的唯一组合。
- *
  * candidates 中的数字可以无限制重复被选取。如果至少一个所选数字数量不同，则两种组合是唯一的。
- *
  * 对于给定的输入，保证和为 target 的唯一组合数少于 150 个。
  *
  * ## 解题思路: 回溯算法
@@ -27,16 +25,17 @@ public class Solution {
         if (idx == candidates.length) {
             return;
         }
-        if (target == 0) {  // one success combination
-            ans.add(new ArrayList<>(combine));  // deep copy
+        if (target == 0) {
+            ans.add(new ArrayList<>(combine));  // remember deep copy
+            return;
         }
-        // skip current candidate number
-        dfs(candidates, target, ans, combine, idx + 1);
-        // try current candidate number
-        if (target - candidates[idx] >= 0) {
+        // try current idx
+        if (candidates[idx] <= target) {
             combine.add(candidates[idx]);
             dfs(candidates, target - candidates[idx], ans, combine, idx);
             combine.remove(combine.size() - 1);
         }
+        // skip current idx
+        dfs(candidates, target, ans, combine, idx + 1);
     }
 }
